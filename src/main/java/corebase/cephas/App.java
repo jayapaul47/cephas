342a0b7eae89308c3f851b39d929a726dd6f4eb9
Jaya Paul Dara <JayaPaul.Dara@XCHANGING.MY.LOCAL>
2020-06-27 22:11:13 +0800

    adding framework

---
+++ src/main/java/corebase/cephas/App.java
@@ -0,0 +1,13 @@
+package corebase.cephas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
